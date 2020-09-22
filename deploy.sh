b7ec19e11736d323902c68cc28d8dcb7934b1e87
fti-dbhosale <dbhosale@ftilab.com>
2020-09-22 15:42:19 +0530

    .

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "Scorer Cloud API deploy script must be executed from the project root directory."
-function dir_error() {
+dir_error() {
     echo "Scorer Cloud API deploy script must be executed from the project root directory."
 }
+dir_error
