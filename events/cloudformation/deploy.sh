389f50c7910a105e12d46af962fffb7da5b68e7f
fti-dbhosale <dbhosale@ftilab.com>
2020-09-24 01:34:10 +0530

    removed uuidgen

--- events/cloudformation/deploy.sh
+++ events/cloudformation/deploy.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-UUID=`uuidgen`
 NOW=`date "+%Y%m%d_%H%M%S"`
-CODE_S3_PREFIX="${NOW}/${UUID}"
+CODE_S3_PREFIX="${NOW}"
 
-echo `hello ${CODE_S3_PREFIX}`
+${CODE_S3_PREFIX}
 aws lambda list-functions
