21afb12d1b97bcdaf097b70a2da5af248bf66156
fti-dbhosale <dbhosale@ftilab.com>
2020-09-23 13:21:59 +0530

    Changes for installing python

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+apk add --update --no-cache python3 && ln -sf python3 /usr/bin/python
+python3 -m ensurepip
+pip3 install --no-cache --upgrade pip setuptools
+pip3 install aws-sam-cli
 cd events/cloudformation
+ls
 python -v
