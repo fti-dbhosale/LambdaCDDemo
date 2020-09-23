6c8b024cbf481bcbc8c5b6fb4bea5aee15986ce2
fti-dbhosale <dbhosale@ftilab.com>
2020-09-24 03:57:05 +0530

    creating zip

--- events/cloudformation/deploy.sh
+++ events/cloudformation/deploy.sh
@@ -1,20 +1,18 @@
-
+#!/usr/bin/env bash
 #AWS_PROFILE_OPTION=`create_aws_profile_option ${AWS_PROFILE}`
 UUID=""
 NOW=`date "+%Y%m%d_%H%M%S"`
 CODE_S3_PREFIX="${NOW}/${UUID}"
-pushd ../
-ls
 
 echo $CODE_S3_PREFIX
 echo $AWS_PROFILE
 #aws lambda list-functions
 CODE_ZIP="events-function.zip"
-ls
-sam --info
-sam build
+ rm ./cloudformation/${CODE_ZIP}
+
  cd ./events/lambda/api
- #run zip -q ../../cloudformation/${CODE_ZIP} \
+ pip3 install -r ./requirements.txt --target ./
+ run zip -q ../../cloudformation/${CODE_ZIP}
  ls
  -r \
  . \
