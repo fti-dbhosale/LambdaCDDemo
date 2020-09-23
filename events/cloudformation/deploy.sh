b09c129ef9362e1fc686e89da957cf0c7d3e2d2c
fti-dbhosale <dbhosale@ftilab.com>
2020-09-24 01:44:37 +0530

    Getting AWS_PROFILE

--- events/cloudformation/deploy.sh
+++ events/cloudformation/deploy.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-
-UUID=`uuidgen`
+AWS_PROFILE_OPTION=`create_aws_profile_option ${AWS_PROFILE}`
+UUID=""
 NOW=`date "+%Y%m%d_%H%M%S"`
 CODE_S3_PREFIX="${NOW}/${UUID}"
 
 echo $CODE_S3_PREFIX
+echo $AWS_PROFILE_OPTION
 #aws lambda list-functions
