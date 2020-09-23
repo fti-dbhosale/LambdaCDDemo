b8140c94cbc59a274cd43e2a558ccd086356afdc
fti-dbhosale <dbhosale@ftilab.com>
2020-09-24 01:37:47 +0530

    Added UUID

--- events/cloudformation/deploy.sh
+++ events/cloudformation/deploy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+UUID=`uuidgen`
 NOW=`date "+%Y%m%d_%H%M%S"`
-CODE_S3_PREFIX="${NOW}"
+CODE_S3_PREFIX="${NOW}/${UUID}"
 
-${CODE_S3_PREFIX}
+CODE_S3_PREFIX
 aws lambda list-functions
