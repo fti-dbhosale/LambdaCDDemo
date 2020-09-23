8c8ec6ef2329c830da2878efae729982a24fa9b9
fti-dbhosale <dbhosale@ftilab.com>
2020-09-23 18:20:24 +0530

    Changes for docker image

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-apk add --update --no-cache python3 && ln -sf python3 /usr/bin/python
-python3 -m ensurepip
-pip3 install --no-cache --upgrade pip setuptools
 pip3 install aws-sam-cli
 cd events/cloudformation
 ls
 python3 hello.py
-
-sam build
