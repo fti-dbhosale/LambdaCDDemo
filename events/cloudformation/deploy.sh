bc23027ba654079a211a9554c0c4da5c8efe8f45
fti-dbhosale <dbhosale@ftilab.com>
2020-09-24 03:09:18 +0530

    installing sam cli

--- events/cloudformation/deploy.sh
+++ events/cloudformation/deploy.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 #AWS_PROFILE_OPTION=`create_aws_profile_option ${AWS_PROFILE}`
-SHELL := /bin/bash
 UUID=""
 NOW=`date "+%Y%m%d_%H%M%S"`
 CODE_S3_PREFIX="${NOW}/${UUID}"
@@ -9,11 +8,9 @@ echo $CODE_S3_PREFIX
 echo $AWS_PROFILE
 #aws lambda list-functions
 CODE_ZIP="events-function.zip"
- pushd ../
- rm ./cloudformation/${CODE_ZIP}
-
+ls
+sam build
  cd ./events/lambda/api
- pip3 install -r ./requirements.txt --target ./
  #run zip -q ../../cloudformation/${CODE_ZIP} \
  ls
  -r \
