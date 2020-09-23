0dbdee2bc5c456ad122d7f53f951d77e5d058420
fti-dbhosale <dbhosale@ftilab.com>
2020-09-24 05:27:09 +0530

    Changes for deploy cloudformation

--- events/cloudformation/deploy.sh
+++ events/cloudformation/deploy.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 #AWS_PROFILE_OPTION=`create_aws_profile_option ${AWS_PROFILE}`
 UUID=""
+STACK_NAME="fti-events"
+LAMBDA_VERSION="1.0"
+REALM="dev"
+INSTANCE="dev"
+DEFAULT_PYTHON_RUNTIME="python3.8"
 NOW=`date "+%Y%m%d_%H%M%S"`
 CODE_S3_PREFIX="${NOW}/${UUID}"
 CODE_UPLOAD_BUCKET="demo-lambda-pipeline"
@@ -19,5 +24,18 @@ CODE_ZIP="events-function.zip"
 cd ../../cloudformation
 ls
 aws --region ${REGION} s3 cp ${CODE_ZIP} s3://${CODE_UPLOAD_BUCKET}/${CODE_S3_PREFIX}/${CODE_ZIP}
-cd ../
-ls
+
+aws --region ${REGION} ${AWS_PROFILE_OPTION} cloudformation deploy \
+    --template-file events.yaml \
+    --s3-bucket ${CODE_UPLOAD_BUCKET} \
+    --s3-prefix ${CODE_S3_PREFIX} \
+    --stack-name ${STACK_NAME} \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --no-fail-on-empty-changeset \
+    --parameter-overrides \
+    InstanceName=${INSTANCE} \
+    Version=${LAMBDA_VERSION} \
+    LambdaCodeS3Bucket=${CODE_UPLOAD_BUCKET} \
+    LambdaCodeS3Key="${CODE_S3_PREFIX}/${CODE_ZIP}" \
+    RealmName=${REALM} \
+    PythonRuntime=${DEFAULT_PYTHON_RUNTIME}
