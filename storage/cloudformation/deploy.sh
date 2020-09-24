f76f8764e7ee59edea43dde27c555563413f8447
fti-dbhosale <dbhosale@ftilab.com>
2020-09-24 07:32:50 +0530

    Added in workflow storage

--- storage/cloudformation/deploy.sh
+++ storage/cloudformation/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #AWS_PROFILE_OPTION=`create_aws_profile_option ${AWS_PROFILE}`
 UUID=""
-STACK_NAME="fti-events"
+STACK_NAME="storage-events"
 LAMBDA_VERSION="1.0"
 REALM="dev"
 INSTANCE="dev"
@@ -10,27 +10,22 @@ NOW=`date "+%Y%m%d_%H%M%S"`
 CODE_S3_PREFIX=${NOW}
 CODE_UPLOAD_BUCKET="demo-lambda-pipeline"
 REGION="ap-south-1"
-CODE_S3_PREFIX="${CODE_S3_PREFIX}.events-function"
-CloudFrontKeyPairFileKey=""
-CloudFrontDeviceImagesBucketDomainName=""
-CloudFrontKeyPairID=""
-CloudFrontKeyPairBucket=""
+CODE_S3_PREFIX="${CODE_S3_PREFIX}.storage-function"
 
-CODE_ZIP="events-function.zip"
-rm ./events/cloudformation/${CODE_ZIP}
-cd ./events/lambda/api
+CODE_ZIP="storage-function.zip"
+rm ./storage/cloudformation/${CODE_ZIP}
+cd ./storage/lambda
 
 pip3 install -r ./requirements.txt --target ./
-cp -r /root/LambdaCDDemo/events/lambda/dispatcher dispatcher
-zip -r ../../cloudformation/${CODE_ZIP} .
+zip -r ../cloudformation/${CODE_ZIP} .
 
 ls
-cd ../../cloudformation
+cd ../cloudformation
 ls
 aws --region ${REGION} s3 cp ${CODE_ZIP} s3://${CODE_UPLOAD_BUCKET}/${CODE_S3_PREFIX}/${CODE_ZIP}
 
 aws --region ${REGION} cloudformation deploy \
-    --template-file events.yaml \
+    --template-file storage.yaml \
     --s3-bucket ${CODE_UPLOAD_BUCKET} \
     --s3-prefix ${CODE_S3_PREFIX} \
     --stack-name ${STACK_NAME} \
@@ -42,7 +37,4 @@ aws --region ${REGION} cloudformation deploy \
     LambdaCodeS3Bucket=${CODE_UPLOAD_BUCKET} \
     LambdaCodeS3Key="${CODE_S3_PREFIX}/${CODE_ZIP}" \
     RealmName=${REALM} \
-    PythonRuntime=${DEFAULT_PYTHON_RUNTIME} \
-    CloudFrontKeyPairID=${CLOUD_FRONT_KEY_PAIR_ID} \
-    CloudFrontKeyPairBucket=${CLOUD_FRONT_KEY_PAIR_BUCKET} \
-    CloudFrontKeyPairFileKey=${CLOUD_FRONT_KEY_PAIR_FILE_KEY}
+    PythonRuntime=${DEFAULT_PYTHON_RUNTIME}
