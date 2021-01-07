0b134facb4181fb73d58baff326f5e18b4d45e65
dbhosale-fti <dbhosale@ftilab.com>
2021-01-07 17:32:10 +0530

    Update for sqs send message

--- events/lambda/dispatcher/dynamo_db_split_record.py
+++ events/lambda/dispatcher/dynamo_db_split_record.py
@@ -6,8 +6,14 @@ from datetime import datetime, timedelta
 from boto3.dynamodb.types import TypeDeserializer, TypeSerializer
 from boto3.dynamodb.conditions import Key, And
 
-
+sqs = boto3.client('sqs')
+queue_url = os.environ['TARGET_SQS']
 def lambda_handler(event, context):
     logging.getLogger().setLevel(logging.INFO)
     print(event)
-    return True
+    for record in event['Records']:
+        if 'aws:sns' == record['EventSource'] and record['Sns']['Message']:
+            response = sqs.send_message(
+                QueueUrl=queue_url,
+                DelaySeconds=10,
+                MessageBody=(record['Sns']['Message']))
